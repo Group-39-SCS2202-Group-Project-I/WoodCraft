18591bb9d52dbf4a13405fdc707bfeb7aaec1bc5
Lasith Ranahewa <lasithmrana@gmail.com>
2023-12-29 21:36:12 +0530

    add product categories

--- app/views/admin/customers.view.php
+++ app/views/admin/customers.view.php
@@ -1,65 +1,87 @@
 <?php include "inc/header.view.php"; ?>
 
-<h1>Customers</h1>
 
-<!-- [{"customer_id":1,"user_id":6,"first_name":"Lasith","last_name":"Ranahewa","telephone":"0766716265","address_id":1,"email":"lasithmrana@gmail.com","role":"customer","address_line_1":"150\/A","address_line_2":"Kandy Road","city":"Kadawatha","zip_code":"11850"},{"customer_id":3,"user_id":8,"first_name":"Sasanka","last_name":"Udana","telephone":"0987654321","address_id":3,"email":"sasa@gmail.com","role":"customer","address_line_1":"100\/A","address_line_2":"Kottawa Road","city":"Maharagama","zip_code":"10300"}] -->
 
-<table id="myTable">
-    <tr>
-        <th>Customer ID</th>
-        <th>Name</th>
-        <th>Telephone</th>
-        <th>Email</th>
-        <th>Address ID</th>
-        <th>Zip Code</th>
-        <th>Actions</th>
-    </tr>
+<div class="table-section">
+    <h2 class="table-section__title">Customers</h2>
 
-    <script>
-        document.addEventListener('DOMContentLoaded', function() {
-            function updateTable() {
-                fetch('<?php echo ROOT ?>/fetch/customers')
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log(data);
-                        let table = document.getElementById('myTable');
-                        // Clear existing table rows
-                        while (table.rows.length > 1) {
-                            table.deleteRow(1);
-                        }
-                        // Insert new rows with updated data
-                        data.forEach(item => {
-                            let row = table.insertRow();
-                            let customer_id = "CUS-" + String(item.customer_id).padStart(3, '0');
-                            let name = item.first_name + ' ' + item.last_name;
-                            let addressId = "ADD-" + String(item.address_id).padStart(3, '0');
-                            let zipCode = item.zip_code;
-                            let telephone = item.telephone;
-                            let email = item.email;
+    <div class="table-section__search">
+        <input type="text" id="searchWorkers" placeholder="Search Customers..." class="table-section__search-input">
+    </div>
+    
+    <table class="table-section__table" id="customers_table">
+        <tr>
+            <th>Customer ID</th>
+            <th>Name</th>
+            
+            <th>Email</th>
+            <th>Address</th>
+            <th>Telephone</th>
+            <!-- <th>Address ID</th> -->
+            <!-- <th>Zip Code</th> -->
 
-                            row.insertCell().innerHTML = customer_id;
-                            row.insertCell().innerHTML = name;
-                            row.insertCell().innerHTML = telephone;
-                            row.insertCell().innerHTML = email;
-                            row.insertCell().innerHTML = addressId;
-                            row.insertCell().innerHTML = zipCode;
+            <!-- <th>Actions</th> -->
+        </tr>
 
-                            row.insertCell().innerHTML = `<a href="<?php echo ROOT ?>/admin/customers/${item.customer_id}">View</a> | <a href="<?php echo ROOT ?>/delete/customers/${item.customer_id}">Delete</a>`;
+        <tbody id="table-section__tbody">
+            <script>
+                document.addEventListener('DOMContentLoaded', function() {
+                    function updateTable() {
+                        fetch('<?php echo ROOT ?>/fetch/customers')
+                            .then(response => response.json())
+                            .then(data => {
+                                console.log(data);
+                                let table = document.getElementById('customers_table');
+                                // Clear existing table rows
+                                while (table.rows.length > 1) {
+                                    table.deleteRow(1);
+                                }
+                                // Insert new rows with updated data
+                                data.forEach(item => {
+                                    let row = table.insertRow();
+                                    let customer_id = "CUS-" + String(item.customer_id).padStart(3, '0');
+                                    let name = item.first_name + ' ' + item.last_name;
+                                    let addressId = "ADD-" + String(item.address_id).padStart(3, '0');
+                                    let zipCode = item.zip_code;
+                                    let telephone = item.telephone;
+                                    let email = item.email;
 
-                        });
-                    })
-                    .catch(error => console.error(error));
-            }
+                                    let address_line_1 = item.address_line_1;
+                                    let address_line_2 = item.address_line_2;
+                                    let city = item.city;
 
-            // Initial table update
-            updateTable();
+                                    let address = item.address_line_1 + ',<br>' + item.address_line_2 + ',<br>' + item.city + '.<br>' + item.zip_code;
 
-            // Schedule periodic table updates
-            setInterval(updateTable, 5000); // Update every 5 seconds
-        });
-    </script>
-   
-</table>
+                                    row.insertCell().innerHTML = customer_id;
+                                    row.insertCell().innerHTML = name;
+                                    
+                                    row.insertCell().innerHTML = email;
+                                    row.insertCell().innerHTML = address;
+                                    row.insertCell().innerHTML = telephone;
+                                    // row.insertCell().innerHTML = addressId;
+                                    // row.insertCell().innerHTML = zipCode;
+
+                                    // row.insertCell().innerHTML = `<a class="table-section__button" href="<?php echo ROOT ?>/admin/customers/${item.customer_id}">View</a> <a href="<?php echo ROOT ?>/delete/customers/${item.customer_id}">Delete</a>`;
+                                    // row.insertCell().innerHTML = `<a class="table-section__button" href="<?php echo ROOT ?>/admin/customers/${item.customer_id}">View</a>`;
+
+                                });
+                            })
+                            .catch(error => console.error(error));
+                    }
+
+                    // Initial table update
+                    updateTable();
+
+                    // Schedule periodic table updates
+                    // setInterval(updateTable, 5000); // Update every 5 seconds
+                });
+            </script>
+        </tbody>
+
+    </table>
+
+</div>
+
 
 
 
--- app/views/admin/product_categories.view.php
+++ app/views/admin/product_categories.view.php
@@ -14,59 +14,98 @@ if (isset($_SESSION['errors']) && isset($_SESSION['form_data']) && isset($_SESSI
 }
 ?>
 
-<h1>Product Categories</h1>
-<!-- form to add product category -->
-<form action="<?php echo ROOT ?>/add/product_category" method="post" id="category_form">
+<style>
+    #cat-inp {
+        padding: 10px;
+        margin-bottom: 20px;
+        border: none;
+        border-radius: 5px;
+        background-color: white;
+        font-size: 1rem;
+        width: 60%;
+        /* box-shadow: 0 0 5px rgba(0, 0, 0, 0.1); */
+    }
 
+    #cat-inp:focus {
+        outline: none;
+        border: 2px solid var(--primary);
+    }
+
+    .cat-add-btn {
+        margin-right: 0px;
+        width: 30%;
+    }
+</style>
+
+
+<div class="table-section">
+
+    <!-- form to add product category -->
     <?php if (message()) : ?>
         <div class="mzg-box">
             <div class="messege"><?= message('', true) ?></div>
         </div>
     <?php endif; ?>
 
-    <div class="form-group">
-        <label for="category_name">Category Name</label>
-        <input type="text" name="category_name" id="category_name" class="form-control" value="<?php echo isset($form_data['category_name']) ? $form_data['category_name'] : ''; ?>">
-        <span class="text-danger"><?php echo isset($errors['category_name']) ? $errors['category_name'] : ''; ?></span>
-    </div>
-    <div class="form-group">
-        <input type="submit" value="Add Category" class="btn btn-primary">
-    </div>
-</form>
+    <div style="display: flex;">
+        <div class="flex-half"></div>
+        <div class="flex-half">
 
+            <?php if (!empty($errors['category_name'])) : ?>
+                <p style="display: flex; justify-content: flex-end; padding:0; margin-bottom: 0.5rem" class="validate-mzg"><?= $errors['category_name'] ?></p>
+            <?php endif; ?>
+            <form action="<?php echo ROOT ?>/add/product_category" method="post" id="category_form" style="display: flex; justify-content: flex-end; padding:0">
 
-
-<!-- fetch and display product categories in a list-->
-<div class="category-list">
-    <?php
-    $url = ROOT . "/fetch/product_categories";
-    $response = file_get_contents($url);
-    $data = json_decode($response, true);
-    // show($data);
-    $categories = [];
-    foreach ($data as $item) {
-        $categories[strtoupper(substr($item['category_name'], 0, 1))][] = $item;
-    }
-    ksort($categories);
-    ?>
-    <?php foreach ($categories as $letter => $categoryList) : ?>
-        <div class="category-group">
-            <h2><?php echo $letter; ?></h2>
-            <ul>
-                <?php foreach ($categoryList as $category) : ?>
-                    <li>
-                        <span class="category-id">CAT-<?php echo str_pad($category['product_category_id'], 3, '0', STR_PAD_LEFT); ?></span>
-                        <span class="category-name"><?php echo $category['category_name']; ?></span>
-                        <span class="category-actions">
-                            <a href="<?php echo ROOT ?>/delete/product_categories/<?php echo $category['product_category_id'] ?>" class="btn btn-danger">Delete</a>
-                        </span>
-
-
-                    </li>
-                <?php endforeach; ?>
-            </ul>
+                <input type="text" value="<?= set_value('category_name') ?>" name="category_name" placeholder="Enter Category Name" id="cat-inp">
+                <span style="margin-left: 10px;"></span>
+                <Button type="submit" class="form-btn submit-btn cat-add-btn" style="height: 44px;">Add Category</Button>
+            </form>
         </div>
-    <?php endforeach; ?>
+    </div>
+
+    <h1 class="list-section__title">Product Categories </span></h1>
+
+
+    <div class="table-section__search">
+        <input type="text" id="searchCategories" placeholder="Search Categories..." class="table-section__search-input">
+        <!-- hidden submit button to avoid -->
+        <input type="submit" id="hiddenSubmit" style="display: none;">
+    </div>
+
+
+
+    <!-- fetch and display product categories in a list-->
+    <div class="category-list">
+        <?php
+        $url = ROOT . "/fetch/product_categories";
+        $response = file_get_contents($url);
+        $data = json_decode($response, true);
+        // show($data);
+        $categories = [];
+        foreach ($data as $item) {
+            $categories[strtoupper(substr($item['category_name'], 0, 1))][] = $item;
+        }
+        ksort($categories);
+        ?>
+        <?php foreach ($categories as $letter => $categoryList) : ?>
+            <div class="category-group">
+                <h2><?php echo $letter; ?></h2>
+                <ul>
+                    <?php foreach ($categoryList as $category) : ?>
+                        <li>
+                            <span class="category-id">CAT-<?php echo str_pad($category['product_category_id'], 3, '0', STR_PAD_LEFT); ?></span>
+                            <span class="category-name"><?php echo $category['category_name']; ?></span>
+                            <span class="category-actions">
+                                <a href="<?php echo ROOT ?>/delete/product_categories/<?php echo $category['product_category_id'] ?>" class="btn btn-danger">Delete</a>
+                            </span>
+
+
+                        </li>
+                    <?php endforeach; ?>
+                </ul>
+            </div>
+        <?php endforeach; ?>
+    </div>
 </div>
 
 <?php include "inc/footer.view.php"; ?>
