89c1692734026a1e3aa4bcb32a82db8ac3622cd4
GRPS GANEGODA <piumisarangag@gmail.com>
2024-04-08 09:06:54 +0530

    add all cart page css to the cart.view.php

--- app/views/cart/cart.view.php
+++ app/views/cart/cart.view.php
@@ -157,8 +157,220 @@ $this->view('includes/header', $data) ?>
   }
   .cartdetails{
     display: flexbox;
+
+  }
+  
+ 
+.container-cart {
+    display: flex;
+    margin: 100px 150px 0 150px;
+    flex-direction: row;
+    padding-bottom: 1rem;
+   }
+ .head-img {
+    text-align: left;
+    padding-left: 7px;
+  }
+  
+  .input {
+    border: none;
+    outline: none;
+    width: 100%;
+    height: 40px;
+    margin: 5px 0;
+    padding: 7px;
+  }
+  
+  .cart {
+    width: 55%;
+    border: 1px solid #bdbdbd;
+     display:flex;
+      
+    border-radius: .5rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-content: center;
+  }
+  
+ 
+.fa-trash{
+    color:red;
+}
+ .fa-trash:hover{
+    color: #688272;
+ }  
+ .quantity{
+  display: flex;
+  align-items: center;
+ }
+ .quantity input{
+  width: 30px;
+  text-align: center;
+ }
+ 
+
+
+.summary {
+    width: 45%;
+    border: 1px solid #bdbdbd;
+    padding: 10px;
+    margin-bottom: 20px;
+    border-radius: 2rem;
+    margin-left: 10%;
+    height: 50%;
+    
+}
+  
+  .top {
+    display: flex;
+    justify-content: space-between;
+    padding: 10px;
+    margin: 20px 0;
+  }
+  
+  
+  .detail {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end; 
+  }
+  
+  .detail h2 {
+    display: flex;
+    justify-content: space-between;
+    width: 100%;
+    margin: 5px 0; 
+    font-size: 1vw;
+    color: #333;
+    font-weight: lighter;
+  }
+  
+  .detail h2 span {
+    color: #333;
+  }
+  .detail h2#discount span {
+    color:red;
+  }
+  
+  
+  .detail hr {
+    width: 100%;
+    border: 1px solid #ddd; 
+    margin: 10px 0; 
+  }
+  
+ .promo{
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+ }
+  
+ 
+  
+  .button {
+   
+    background-color:#000;
+    color: #fff;
+    padding: 8px 30px;
+   
+    cursor: pointer;
+    margin-bottom: 15px;
+    border-radius: 15rem;
+    margin-top: 2rem;
+  }
+  .remove {
+   
+    border: none;
+  }
+  
+  .checkout {
+    width: 100%;
+    background-color:#000;
+    color: #fff;
+    border-radius: 15rem;
+    margin-top: 1rem;
+    height: 2.5rem;
+  }
+  .promocode input[type="text"] {
+    border: 1px solid #ccc; /* Add a border */
+    border-radius: 15rem;
+    height: 2rem;
     
   }
+  .first-btn{
+    width: 20%;
+    height: 2.5rem;
+    background-color:#000;
+    color: #fff;
+    border-radius: 15rem;
+   
+  }
+  
+  button:hover,
+  .checkout:hover {
+    background-color:  #688272;
+  }
+
+  .product {
+    display: flex;
+    flex-direction: row;
+   
+     padding: 10px;
+    width: 100%;
+    justify-content: space-around;
+   
+  }
+  
+  .product-info {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+  
+  .product-details {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items:start;
+  }
+  .product-details p {
+    line-height: 1.5; 
+    margin-bottom: 5px; 
+    display:block;
+    font-size: 1vw;
+    font-weight: lighter;
+    color: #333;
+    
+  }
+  .product-details .title{
+    line-height: 1.5; 
+    margin-bottom: 5px; 
+    display:block;
+    font-size: 1vw;
+    font-weight:bolder;
+    color: #000;
+    
+  }
+  hr {
+    margin: 5px 0;
+    border: 1px solid #ddd;
+  }
+  
+  .quantity {
+    display: flex;
+    border-radius: 2rem;
+    
+
+  }
+  
+  
+  
+ 
+
+
+
+
   
 </style>
 </html>
