f81f224618cb4d27e1c99c72ae0353573590e63f
Lasith Ranahewa <lasithmrana@gmail.com>
2024-04-13 10:44:17 +0530

    osr routes

--- app/controllers/OSR.php
+++ app/controllers/OSR.php
@@ -57,4 +57,79 @@ class OSR extends Controller
             }
         }
     }
+
+    public function orders($id = '')
+    {
+
+        if (!Auth::logged_in()) {
+            message('Please login to view the OSR section');
+            redirect('login');
+        }
+
+        if (!Auth::is_osr()) {
+            $this->view('404');
+        } else {
+            $data['id'] = $id;
+            // if ($id == '') {
+            //     $data['title'] = "Orders";
+            //     $this->view('osr/orders', $data);
+            // } else {
+            //     $data['title'] = "Order";
+            //     $this->view('osr/order', $data);
+            // }
+            $data['title'] = "Orders";
+            $this->view('osr/orders', $data);
+        }
+    }
+
+    // products
+    public function products($id = '')
+    {
+
+        if (!Auth::logged_in()) {
+            message('Please login to view the OSR section');
+            redirect('login');
+        }
+
+        if (!Auth::is_osr()) {
+            $this->view('404');
+        } else {
+            // $data['id'] = $id;
+            // if ($id == '') {
+            //     $data['title'] = "Products";
+            //     $this->view('osr/products', $data);
+            // } else {
+            //     $data['title'] = "Product";
+            //     $this->view('osr/product', $data);
+            // }
+            $data['title'] = "Products";
+            $this->view('osr/products', $data);
+        }
+    }
+
+    // productions
+    public function productions($id = '', $start_date = '', $end_date = '')
+    {
+
+        if (!Auth::logged_in()) {
+            message('Please login to view the OSR section');
+            redirect('login');
+        }
+
+        if (!Auth::is_osr()) {
+            $this->view('404');
+        } else {
+            $data['id'] = $id;
+
+            // if ($id == '') {
+            //     $data['title'] = "Productions";
+            //     $this->view('osr/productions', $data);
+            // } else {
+            //     $data['title'] = "Production";
+            //     $this->view('osr/production', $data);
+            // }
+            $data['title'] = "Productions";
+            $this->view('osr/productions', $data);
+        }
+    }
 }
---
+++ app/views/osr/products.view.php
@@ -0,0 +1,3 @@
+<?php include "inc/header.view.php"; ?>
+<?php echo "products" ?>
+<?php include "inc/footer.view.php"; ?>
