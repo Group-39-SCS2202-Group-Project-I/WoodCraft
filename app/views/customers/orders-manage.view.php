3e7813bd0bc833b2d53b38a811029dadee4a5423
Anuththara93 <136674526+Anuththara93@users.noreply.github.com>
2024-04-24 23:26:44 +0530

    orders-manage view css

--- app/views/customers/orders-manage.view.php
+++ app/views/customers/orders-manage.view.php
@@ -1,23 +1,26 @@
 <style>
+    .content-manage-order {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+        margin-top: 30px;
+        background-color: var(--light);
+        padding: 20px;
+        border-radius: 10px;
+    }
+    
     .content-manage-order-left {
         display: flex;
         flex-direction: column;
         align-items: flex-start;
     }
 
-    .content-manage-order {
-        display: flex;
-        justify-content: space-between;
-        align-items: center;
-        margin-bottom: 20px;
-        background-color: whitesmoke;
-        padding: 10px;
-    }
-
     .content-manage-detail {
         margin-bottom: 20px;
-        background-color: whitesmoke;
-        padding: 10px;
+        background-color: var(--light);
+        padding: 20px;
+        border-radius: 10px;
     }
 
     .content-manage-detail-mid {
@@ -25,13 +28,15 @@
         flex-direction: column;
         align-items: center;
         margin: 20px;
-        background-color: white;
-        padding: 10px;
+        background-color: var(--white);
+        padding: 20px;
+        border-radius: 10px;
     }
 
     .content-manage-detail-mid p {
         font-size: 16px;
         font-style: italic;
+        color: var(--green2);
     }
 
     .order-item {
@@ -64,56 +69,47 @@
 
     .content-manage-payment-left {
         flex: 1;
-        background-color: whitesmoke;
+        background-color: var(--light);
         margin-right: 20px;
-        padding: 10px;
+        padding: 20px;
         align-items: left;
+        border-radius: 10px;
     }
 
     .content-manage-payment-right {
         flex: 1;
-        background-color: whitesmoke;
-        padding: 10px;
+        background-color: var(--light);
+        padding: 20px;
+        border-radius: 10px;
     }
 
     .content-manage-payment-right large {
         font-size: 25px;
-    }
-
-    .right-lower {
-        display: flex;
-        justify-content: space-between;
-        position: relative;
-        padding-bottom: 10px;
-    }
-
-    .right-lower::after {
-        content: '';
-        position: absolute;
-        bottom: 0;
-        left: 0;
-        width: 100%;
-        border-bottom: 1px solid black;
-    }
-
-    .right-lower-lower {
-        display: flex;
-        justify-content: space-between;
-        margin-top: 10px;
-    }
-
-    .right-left {
-        align-items: left;
-    }
-
-    .right-right {
-        align-items: right;
+        color: var(--green2);
+        font-weight: bold;
     }
 
     .content-manage-order-left p small,
     .content-manage-detail p small,
     .content-manage-order p:last-child {
-        color: gray;
+        color: var(--bg1);
+        font-size: 12px;
+    }
+
+    .right-lower h2 {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        width: 100%;
+        margin: 5px 0; 
+        font-size: 1em;
+        font-weight: lighter;
+    }
+
+    .right-lower h2 span {
+        margin-left: 20px;
+        display: flex;
+        align-items: center;
     }
 </style>
 
@@ -133,7 +129,7 @@
             <?php foreach ($data['order_details'] as $details) : ?>
                 <div class="content-manage-order">
                     <div class="content-manage-order-left">
-                        <p>Order <?= $details['order_details_id']; ?></p>
+                        <p>Order <strong style="color: var(--green2);"><?= $details['order_details_id']; ?></strong></p>
                         <p><small>Placed on <?= $details['created_at']; ?></small></p>
                     </div>
                     <p>Total: Rs.<?= $details['total']; ?></p>
@@ -160,7 +156,7 @@
 
                 <div class="content-manage-payment">
                     <div class="content-manage-payment-left">
-                        <h4>Shipping Address</h4><br>
+                        <h4>Default Shipping Address</h4><br>
                         <p><?= $details['first_name']; ?> <?= $details['last_name']; ?></p> 
                         <p><?= $details['city']; ?></p>
                         <p><?= $details['address_line_1']; ?>, <?= $details['address_line_2']; ?></p>
@@ -170,22 +166,10 @@
                     <div class="content-manage-payment-right">
                         <h3>Total Summary</h3><br>
                         <div class="right-lower">
-                            <div class="right-left">
-                                <p>Subtotal</p>
-                                <p>Delivery Fee</p>
-                            </div>
-                            <div class="right-right">
-                                <p>Rs.<?= $data['total_subtotal']; ?></p>
-                                <p>Rs.<?= $details['delivery_cost']; ?></p>
-                            </div>
-                        </div>
-                        <div class="right-lower-lower">
-                            <div class="right-left">
-                                <p>Total</p>
-                            </div>
-                            <div class="right-right">
-                                <p><large>Rs.<?= $details['total']; ?></large></p>
-                            </div>
+                            <h2 id="subtotal">Subtotal<span>Rs. <?= $data['total_subtotal']; ?></span></h2>
+                            <h2 id="delivery">Delivery<span>Rs. <?= $details['delivery_cost']; ?></span></h2>
+                            <hr>
+                            <h2 id="total">Total<large><span>Rs. <?= $details['total']; ?></span></large></h2>
                         </div>
                     </div>
                 </div>
